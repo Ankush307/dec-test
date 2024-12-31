f23c0093d149085eb9fd38ae23b647bb6c5c4c23
Ankush307 <ankushdhull966@gmail.com>
2024-12-31 12:49:17 +0530

    add code

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -11,11 +11,11 @@ const Cards = () => {
     };
 
     return (
-        <div className='d-flex align-items-center gap-4 justify-content-center h-screen  flex-column flex-xl-row gap-3 px-4'>
+        <div className='d-flex align-items-center gap-4 justify-content-center h-screen  flex-column flex-xl-row gap-3 pb-5 pt-lg-0 pt-3 px-4'>
             {CARDS_DATA_LIST.map((obj, i) => (
-                <div key={i} onClick={() => clickHandler(obj.heading)} className='card p-4'>
+                <div key={i} onClick={() => clickHandler(obj.heading)} className='card p-lg-4 p-3'>
                     <img src={obj.img} alt="img" className='param-cards-img w-100' />
-                    <h2 className='text-center py-4'>
+                    <h2 className='text-center pt-lg-4 pt-3 m-0'>
                         {obj.heading}
                     </h2>
                 </div>
