91e3e425f95fafdd4475f8858a9e45953e249c81
Ankush307 <ankushdhull966@gmail.com>
2024-12-31 14:36:06 +0530

    change name

--- src/components/Competition.jsx
+++ src/components/Competition.jsx
@@ -13,16 +13,16 @@ const Competition = () => {
                         <p className="mt-sm-2 mt-2 m-0 text-white description"> The current return and refund process centres around either traditional advice or software. By combining both together for an aggressively affordable fee, we think we can capture much of the demand for those people that feel competent in managing their own refunds / returns.</p>
                         <p className="mt-sm-2 mt-2 m-0 text-white description">  As well as being the cheapest in market, our product offers everything a sole trader needs  in one place, whilst pricing innovation of zero commission rather than a commission, will  drive customers that are particularly price sensitive.</p>
                     </div>
-                    <div className='col-xl-6 table-responsive d-flex table-bg border-radius-xl'>
+                    <div className='col-xl-6 table-responsive d-flex bg-deep-sea-blue border-radius-xl'>
                         <div className='d-flex align-items-center justify-content-center table-y-spacing'>
-                            <table className='Competition-table'>
+                            <table className='mobile-table'>
                                 <thead>
                                     <tr>
                                         <th></th>
                                         <th className='border-top-radius text-center bg-white fw-semibold table-heading'>Rift</th>
                                         <th className='border-top-radius text-center bg-white fw-semibold table-heading'>Taxscout</th>
                                         <th className='border-top-radius text-center bg-white fw-semibold table-heading'>Coconut</th>
-                                        <th className='border-top-radius  text-center bg-lightGreen fw-semibold table-heading'>Pie</th>
+                                        <th className='border-top-radius  text-center bg-aqua-marine fw-semibold table-heading'>Pie</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -32,7 +32,7 @@ const Competition = () => {
                                             <td className={`text-center ${i === 5 ? 'fees-year border-bottom-radius' : ''} bg-white table-data custom-border-b-none`}>{obj.rift}</td>
                                             <td className={`text-center ${i === 5 ? 'fees-year border-bottom-radius' : ''} bg-white table-data custom-border-b-none`}>{obj.taxscout}</td>
                                             <td className={`text-center ${i === 5 ? 'fees-year border-bottom-radius' : ''} bg-white table-data custom-border-b-none`}>{obj.coconut}</td>
-                                            <td className={`text-center bg-lightGreen table-data custom-border-b-none ${i === 5 ? 'fees-year border-bottom-radius' : ''}`}>{obj.pie}</td>
+                                            <td className={`text-center bg-aqua-marine table-data custom-border-b-none ${i === 5 ? 'fees-year border-bottom-radius' : ''}`}>{obj.pie}</td>
                                         </tr>
                                     ))}
                                 </tbody>
